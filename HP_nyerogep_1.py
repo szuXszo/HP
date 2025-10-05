c3d75bb688b1ab5397bdeb0ae3df26cb62d582d8
Peter Horvath <horvathpeti99@gmail.com>
2025-10-05 18:52:06 +0200

    adding poems

---
+++ HP_nyerogep_1.py
@@ -0,0 +1,16 @@
+from random import choice
+
+
+print("NYERŐGÉP 1\n")
+
+print(choice(["arannyal        ", "keserűséggel    ", "hittel          "]),
+      choice(["érkezik         ", "távozik         "]),
+      choice(["a pokolból      ", "a lakásodból    ", "a tengerpartról "]))
+
+print(choice(["érkezik         ", "távozik         "]),
+      "mint bukott     ",
+      choice(["angyal          ", "ördög           "]))
+
+print(choice(["a pokolba       ", "a lakásodba     ", "a tengerpartra  "]),
+      choice(["angyal          ", "ördög           "]),
+      choice(["érkezik         ", "távozik         "]))
---
+++ HP_nyerogep_2.py
@@ -0,0 +1,16 @@
+from random import choice
+
+
+print("NYERŐGÉP 2 – transzgender verzió\n")
+
+print(choice(["arannyal        ", "keserűséggel    ", "hittel          "]),
+      choice(["érkezik         ", "távozik         "]),
+      choice(["a kórteremből   ", "a lakásodból    ", "a miséről       "]))
+
+print(choice(["érkezik         ", "távozik         "]),
+      "mint bukott     ",
+      choice(["férfi           ", "nő              "]))
+
+print(choice(["a kórterembe    ", "a lakásodba     ", "a misére        "]),
+      choice(["férfi           ", "nő              "]),
+      choice(["érkezik         ", "távozik         "]))
