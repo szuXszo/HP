f1c682fdaa72796f5795afc8968ce0f4457f95ba
Peter Horvath <horvathpeti99@gmail.com>
2025-12-03 00:05:14 +0100

    adding poem

---
+++ HP_toredek.py
@@ -0,0 +1,19 @@
+from random import choice
+
+
+print("TÖREDÉK\n")
+print(f'{choice(["Engem", "Téged", "Minket", "Őt", "Bárkit", "Valaki", "Valami"])} '
+      f'{choice(["meg", "le", "elt", "ki", "széts", "át", "tönkre", "bef"])}')
+print(f'{choice(["mégis", "mint", "ahogy", "és", "de", "miután", "hiába"])}')
+print(f'{choice(["A klubbok", "A tabletták", "A remegés", "Az idegenek", "A véráldozat", "A vasárnap", "Az újév", "A poharad", "A reflektorok"])}')
+print(f'{choice(["apa", "anya", "a csövesek", "kutyák", "galambok", "a szél", "a kamionokból", "a legyekkel", "a síelőknek"])}\n')
+
+print(f'{choice(["Kéretlen", "Végtelen", "Hegyes", "Sima", "Felizzó", "Nyálkás", "Kifordult", "Szagtalan", "Botcsinálta"])}')
+print(f'{choice(["öngyújtóval", "a kabátodban", "ágyunkban", "a képernyő", "a zebrán", "ez az ország", "tegnap", "soha"])} '
+      f'{choice(["a", "az", "egy", "se"])}')
+print(f'{choice(["Hátranéz", "Meglátlak", "Már emlékszik", "Megismersz", "Rádnevet", "Felemelsz", "Odalépek", "Elbotlik"])}')
+print(f'mégsem {choice("ertzuiopőúóüöasdfghjkléáűícvbnm")}')
+
+
+
+
